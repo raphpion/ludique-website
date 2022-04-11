81d9b7c3fe95687bbc20c940cc2a4711d01bf46e
Raphaël Pion <raph.pion@gmail.com>
2022-04-11 09:01:13 -0400

    Fix navbar didn't collapse

--- components/navbar.php
+++ components/navbar.php
@@ -6,16 +6,17 @@ $links = get_navbar_links();
   <div class="container">
     <a class="navbar-brand" href="<?php the_home_url() ?>">Ludique Gaming</a>
     <?php if ( !empty( $links ) ): ?>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
           <?php foreach ( $links as $link ): ?>
             <li class="nav-item">
               <a class="nav-link<?php the_active_class( $link->get_slug() ) ?>" href="<?php $link->the_permalink() ?>"><?php echo $link->the_title() ?></a>
             </li>
           <?php endforeach ?>
+        </div>
       </div>
     <?php endif ?>
   </div>
