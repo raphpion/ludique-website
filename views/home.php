a30ffbc943a8a2a274faddc1bdef0fe0fce56833
Raphaël Pion <raph.pion@gmail.com>
2022-04-11 14:24:16 -0400

    Add Fontawesome, home hero

--- views/home.php
+++ views/home.php
@@ -1,1 +1,10 @@
-<h1>Home</h1>
+<section class="home-hero jumbotron jumbotron-fluid">
+  <div class="container">
+    <div class="spacer-100"></div>
+    <h1 class="display-4">Welcome to Ludique Gaming!</h1>
+    <p class="lead">We are a community of gamers based in Québec, Canada but looking to expand.</p>
+    <hr>
+    <a href="https://discord.gg/bg4jtCzWcu" class="home-hero__join-us btn btn-success">Join our Discord <i class="fab fa-discord"></i></i></a>
+    <div class="spacer-100"></div>
+  </div>
+</section>
